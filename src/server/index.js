35ea9c5f35d4f45b452423fb5d6f087090386296
nilskj <nilskjellman@gmail.com>
2022-06-25 21:03:23 +0200

    :bug: fix some colliding dev ports

--- src/server/index.js
+++ src/server/index.js
@@ -6,23 +6,25 @@ const app = express();
 
 // init payload and hook it up to our express server
 payload.init({
-  secret: 'SECRET_KEY',
-  mongoURL: 'mongodb://root:rootpassword@localhost:27017/payload-kit?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
-  express: app,
-})
+	secret: 'SECRET_KEY',
+	mongoURL:
+		'mongodb://root:rootpassword@localhost:27017/payload-kit?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
+	express: app
+});
 
 // add a route that lives separately from the SvelteKit app
 app.get('/healthcheck', (req, res) => {
-  res.end('ok');
+	res.end('ok');
 });
 
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
-// disable if payload needs to be run in standalone.
-if (process.env.CMS_MODE !== "PAYLOAD_STANDALONE") {
-  app.use(handler);
+// disable if payload needs to be run in standalone (for develepment).
+if (process.env.NODE_ENV === "production") {
+  console.log("...merging in built Svelte handler")
+	app.use(handler);
 }
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-
-  console.log('listening on port 3000');
+app.listen(port, () => {
+	console.log(`Server started port:${port}`);
 });
--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,12 @@ const config = {
 	],
 
 	kit: {
-    adapter: adapter({
-      // default options are shown
-      out: 'build',
-      precompress: false,
-      envPrefix: ''
-    })
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			envPrefix: ''
+		})
 	}
 };
 
